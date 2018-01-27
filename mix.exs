7ae2c63cc36ef5f259fde3642f23f7a3e0b55859
Clark Kampfe <clark.kampfe@gmail.com>
2018-01-26 20:05:43 -0700

    add benchee, ex_doc, and package info

--- mix.exs
+++ mix.exs
@@ -6,6 +6,8 @@ defmodule Cbuf.MixProject do
       app: :cbuf,
       version: "0.1.0",
       elixir: "~> 1.6",
+      package: package(),
+      description: "A circular buffer backed by Erlang's array",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
@@ -19,7 +21,14 @@ defmodule Cbuf.MixProject do
 
   defp deps do
     [
-      {:benchee, "~> 0.11", only: :dev}
+      {:benchee, "~> 0.11", only: :dev},
+      {:ex_doc, "~> 0.16", only: :dev, runtime: false}
     ]
+  end
+
+  defp package do
+    [maintainers: ["Clark Kampfe"],
+     licenses: ["MIT"],
+     links: %{"Github" => "https://github.com/ckampfe/cbuf"}]
   end
 end
