1b727e7b22ce0ca31729b8715de701dbf005ff6a
Clark Kampfe <clark.kampfe@gmail.com>
2018-03-05 21:40:06 -0600

    bump 0.6.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Cbuf.MixProject do
   def project do
     [
       app: :cbuf,
-      version: "0.5.0",
+      version: "0.6.0",
       elixir: "~> 1.5",
       package: package(),
       description: "A circular buffer backed by a map or ETS",
